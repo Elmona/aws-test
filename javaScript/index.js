a8e36a908e3f4e6132e875782c7c00d84152c039
Elmona <emil_larsson83@hotmail.com>
2019-05-01 12:15:25 +0200

    Adding JavaScript

---
+++ javaScript/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/**
+ * handler
+ * Empty function
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
+const handler = (event, context, callback) => {
+  callback(null, event)
+}
+
+module.exports = {
+  handler,
+}
