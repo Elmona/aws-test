9be2e852b271bd2493689403f1817a1b3d2cb065
Elmona <emil_larsson83@hotmail.com>
2019-05-08 08:40:31 +0200

    Adding Java

---
+++ java/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HANDLER=io.symphonia.Hello::handler
+LAMBDA_FUNCTION_NAME=javatest
+LAMBDA_FUNCTION_REGION=eu-north-1
+LAMBDA_FUNCTION_ROLE_ARN=arn:aws:iam::364107138694:role/test
+LAMBDA_FUNCTION_RUNTIME=java8
+
+# TODO Add mvn package here
+
+echo "Check if lamba function exists..."
+aws lambda get-function --function-name ${LAMBDA_FUNCTION_NAME} --region ${LAMBDA_FUNCTION_REGION}
+
+if [ "$?" -eq "0" ]; then
+# Lambda exists
+    echo "Updating existing lambda function..."
+    aws lambda update-function-code \
+        --function-name ${LAMBDA_FUNCTION_NAME} \
+        --zip-file fileb://target/original-hello-1.0-SNAPSHOT.jar \
+        --region ${LAMBDA_FUNCTION_REGION}
+else
+# Lambda does not exist yet
+    echo "Creating lambda function..."
+    aws lambda create-function \
+        --function-name ${LAMBDA_FUNCTION_NAME} \
+        --region ${LAMBDA_FUNCTION_REGION} \
+        --runtime=${LAMBDA_FUNCTION_RUNTIME} \
+        --role=${LAMBDA_FUNCTION_ROLE_ARN} \
+        --handler=${HANDLER} \
+        --zip-file fileb://target/original-hello-1.0-SNAPSHOT.jar
+fi
---
+++ java/src/main/java/io/symphonia/Hello.java
@@ -0,0 +1,7 @@
+package io.symphonia;
+
+public class Hello {
+  public String handler() {
+    return "Hello World";
+  }
+}
